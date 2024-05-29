b0639f324515b6b15dd7857f9487ad14c68d3c06
ratigrdzelidze <164761160+ratigrdzelidze@users.noreply.github.com>
2024-05-29 15:34:26 +0400

    Add files via upload

---
+++ leqcia19/script.js
@@ -0,0 +1,14 @@
+// let myname = "rati"
+// let myhobby = "play games, watch movies, play football"
+// let yearofbirth = 2008
+// let currentyear = 2024
+
+// console.log('my name is' + myname + "my hobby is" + myhobby + "my year of birth is" + yearofbirth + "current year is" + currentyear)
+
+
+let ricxvi =[5, 10, 15, 20, 25]
+
+console.log(ricxvi[0] + ricxvi[1] + ricxvi[2] + ricxvi[3] + ricxvi[4]/5)
+ 
+
+
